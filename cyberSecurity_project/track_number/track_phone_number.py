78c171063c68bc0cd72cd43e2f7d072d9be6fa8c
pritesh460 <kalsariyapritesh9999@gmail.com>
2025-08-05 17:39:44 +0530

    track number

---
+++ cyberSecurity_project/track_number/track_phone_number.py
@@ -0,0 +1,29 @@
+import phonenumbers
+from phonenumbers import geocoder
+import folium
+
+Key = "1161f0c7904146a4aa33bef9a6bf50c6"
+
+number = input("enter phone number with country code:")
+check_number = phonenumbers.parse(number)
+number_location = geocoder.description_for_number(check_number, "en")
+print(number_location)
+
+from phonenumbers import carrier
+service_provider = phonenumbers.parse(number)
+print(carrier.name_for_number(service_provider, "en"))
+
+from opencage.geocoder import OpenCageGeocode
+geocoder = OpenCageGeocode(Key)
+
+query = str(number_location)
+results = geocoder.geocode(query)
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+
+print(lat ,lng)
+
+map_location = folium.Map(location = [lat ,lng], zoom_start=9)
+folium.Marker([lat,lng], popup=number_location).add_to(map_location)
+map_location.save("mylocation.html")
